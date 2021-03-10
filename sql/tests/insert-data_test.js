cadf934a1ca2e53540652a2bfdcadb8d0de3fea1
Thomas Le Naour <lenaourthomas@gmail.com>
2021-03-10 17:46:59 +0100

    fix insert-data_test

--- sql/tests/insert-data_test.js
+++ sql/tests/insert-data_test.js
@@ -1,7 +1,7 @@
 export const tests = []
 const t = (f) => tests.push(f)
 
-t(async ({ eq, db }) => eq(
+t(({ eq, db }) => eq(
   db.prepare(`SELECT * FROM playlists WHERE Name = 'lol'`).get(),
   { PlaylistId: 19, Name: 'lol' }
 ))
