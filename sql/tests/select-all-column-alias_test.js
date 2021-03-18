94640bfa5cad47e17dcf07f6154ca5a7db4e341a
Thomas Le Naour <lenaourthomas@gmail.com>
2021-03-18 14:52:49 +0100

    add select-all-column-alias

---
+++ sql/tests/select-all-column-alias_test.js
@@ -0,0 +1,17 @@
+export const tests = []
+const t = (f) => tests.push(f)
+
+t(({ eq, rows }) => eq(rows, expected))
+
+const expected = [
+  { Identifiant: 1, Nom: 'Adams', Prenom: 'Andrew' },
+  { Identifiant: 2, Nom: 'Edwards', Prenom: 'Nancy' },
+  { Identifiant: 3, Nom: 'Peacock', Prenom: 'Jane' },
+  { Identifiant: 4, Nom: 'Park', Prenom: 'Margaret' },
+  { Identifiant: 5, Nom: 'Johnson', Prenom: 'Steve' },
+  { Identifiant: 6, Nom: 'Mitchell', Prenom: 'Michael' },
+  { Identifiant: 7, Nom: 'King', Prenom: 'Robert' },
+  { Identifiant: 8, Nom: 'Callahan', Prenom: 'Laura' }
+]
+
+Object.freeze(tests)
---
+++ subjects/select-all-column-alias/README.md
@@ -0,0 +1,14 @@
+## select-all-column-alias
+
+### Instrutions
+
+Get all rows from table `tracks` only with this specific columns with aliases :
+
+- `EmployeeId` with alias `Identifiant`
+- `LastName` with alias `Nom`
+- `FirstName` with alias `Prenom`
+
+### Notions
+
+- [sql.sh/cours/select](https://sql.sh/cours/select)
+- [sql.sh/cours/alias](https://sql.sh/cours/alias)
