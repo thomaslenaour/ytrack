61c13d6d6ee1ca1ea8403707d0de6c4554328500
Romain Ranaivoson <romain.ranaivoson@ynov.com>
2021-07-12 18:41:05 +0200

    fixed trafficlihgexercice

--- csharp/tests/NextTrafficLightColorTests/TrafficLightColorTest.cs
+++ csharp/tests/NextTrafficLightColorTests/TrafficLightColorTest.cs
@@ -10,19 +10,19 @@ namespace tests
         [TestMethod]
         public void GetNextTrafficLightColor_Red_Green()
         {
-            Assert.AreEqual(TrafficLightColor.Green, NextTrafficLightColor_Exercise.GetNextTrafficLightColor(TrafficLightColor.Red), "La couleur qui suit le rouge doit être le vert");
+            Assert.AreEqual(TrafficLightColor.Green, NextTrafficLightColor_Exercice.GetNextTrafficLightColor(TrafficLightColor.Red), "La couleur qui suit le rouge doit être le vert");
         }
 
         [TestMethod]
         public void GetNextTrafficLightColor_Green_Orange()
         {
-            Assert.AreEqual(TrafficLightColor.Orange, NextTrafficLightColor_Exercise.GetNextTrafficLightColor(TrafficLightColor.Green), "La couleur qui suit le vert doit être le orange");
+            Assert.AreEqual(TrafficLightColor.Orange, NextTrafficLightColor_Exercice.GetNextTrafficLightColor(TrafficLightColor.Green), "La couleur qui suit le vert doit être le orange");
         }
 
         [TestMethod]
         public void GetNextTrafficLightColor_Orange_Red()
         {
-            Assert.AreEqual(TrafficLightColor.Red, NextTrafficLightColor_Exercise.GetNextTrafficLightColor(TrafficLightColor.Orange), "La couleur qui suit le orange doit être le rouge");
+            Assert.AreEqual(TrafficLightColor.Red, NextTrafficLightColor_Exercice.GetNextTrafficLightColor(TrafficLightColor.Orange), "La couleur qui suit le orange doit être le rouge");
         }
     }
 }
