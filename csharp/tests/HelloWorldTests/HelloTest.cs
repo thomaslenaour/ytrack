5bd765f400b7ab80d8587cd1f8ce3d20f979e61d
Romain Ranaivoson <romain.ranaivoson@ynov.com>
2021-07-12 14:13:48 +0200

    added "!" to hellowrld test

--- csharp/tests/HelloWorldTests/HelloTest.cs
+++ csharp/tests/HelloWorldTests/HelloTest.cs
@@ -10,7 +10,7 @@ namespace tests
         public void Hello_Nominal_ReturnsHelloWorld()
         {
             var result = HelloWorld_Exercice.HelloWorld();
-            Assert.AreEqual("Hello World", result, "La chaîne de caractères retournée n'est pas égale à \"Hello World\"");
+            Assert.AreEqual("Hello World !", result, "La chaîne de caractères retournée n'est pas égale à \"Hello World !\"");
         }
     }
 }
