f4251d0202dc5351fc75bd8dddb70c66d51e981e
Romain Ranaivoson <romain.ranaivoson@ynov.com>
2021-07-12 15:46:39 +0200

    fixed isodd test

--- csharp/tests/IsOddtests/IsOddtest.cs
+++ csharp/tests/IsOddtests/IsOddtest.cs
@@ -1,31 +1,31 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CSharpDiscovery.Quest01;
 
-namespace CSharpDiscoveryTests.Quest01Tests
+namespace tests
 {
     [TestClass]
-    public class _03_IsOddTests
+    public class IsOddTests
     {
         [TestMethod]
         public void IsOdd_OddInteger_True()
         {
-            Assert.IsTrue(_03_IsOdd.IsOdd(34));
+            Assert.IsTrue(IsOdd_Exercice.IsOdd(34));
         }
 
         [TestMethod]
         public void IsOdd_EvenInteger_False()
         {
-            Assert.IsFalse(_03_IsOdd.IsOdd(33));
+            Assert.IsFalse(IsOdd_Exercice.IsOdd(33));
         }
         [TestMethod]
         public void IsOdd_EvenInteger_Zero()
         {
-            Assert.IsTrue(_03_IsOdd.IsOdd(0));
+            Assert.IsTrue(IsOdd_Exercice.IsOdd(0));
         }
         [TestMethod]
         public void IsOdd_EvenInteger_One()
         {
-            Assert.IsFalse(_03_IsOdd.IsOdd(1));
+            Assert.IsFalse(IsOdd_Exercice.IsOdd(1));
         }
     }
 }
