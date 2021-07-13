cb24d454bc0a1e76af3aa31e3b3a930de8828d63
Gauthier <gauthier.michon@ynov.com>
2021-07-13 11:31:38 +0200

    ConcatEverything

---
+++ csharp/tests/ConcatEverythingTests/ConcatEverythingTest.cs
@@ -0,0 +1,21 @@
+﻿using CSharpDiscovery.Quest01;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests
+{
+    [TestClass]
+    public class ConcatEverythingTests
+    {
+        [TestMethod]
+        public void ConcatEverything_tableWithStrings_ConcatedString()
+        {
+            Assert.AreEqual("successfull", ConcatEverything_Exercice.ConcatEverything(new string[] { "success", "full" }), "Cette méthode doit concaténer les différentes chaînes du tableau.");
+        }
+
+        [TestMethod]
+        public void ConcatEverything_tableWithEmptyStrings_ConcatedString()
+        {
+            Assert.AreEqual("success", ConcatEverything_Exercice.ConcatEverything(new string[] { "success", "" }), "Cette méthode doit concaténer les différentes chaînes du tableau.");
+        }
+    }
+}
