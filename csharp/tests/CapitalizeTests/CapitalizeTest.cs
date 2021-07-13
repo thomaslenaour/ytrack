85f09b667a95d5ef156fb504c6a47aff3956ab27
Gauthier <gauthier.michon@ynov.com>
2021-07-13 11:14:17 +0200

    Capitalize + repair GetStringSizeTests

---
+++ csharp/tests/CapitalizeTests/CapitalizeTest.cs
@@ -0,0 +1,15 @@
+﻿using CSharpDiscovery.Quest01;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests
+{
+    [TestClass]
+    public class CapitalizeTests
+    {
+        [TestMethod]
+        public void Capitalize_Ynov_YNOV()
+        {
+            Assert.AreEqual("YNOV", Capitalize_Exercice.Capitalize("Ynov"));
+        }
+    }
+}
