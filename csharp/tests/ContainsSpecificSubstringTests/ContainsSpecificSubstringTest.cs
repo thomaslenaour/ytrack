1af312ca3585f41b476a848cad5984bda52634e4
Gauthier <gauthier.michon@ynov.com>
2021-07-13 11:38:57 +0200

    ContainsSpecificSubstring

---
+++ csharp/tests/ContainsSpecificSubstringTests/ContainsSpecificSubstringTest.cs
@@ -0,0 +1,21 @@
+﻿using CSharpDiscovery.Quest01;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests
+{
+    [TestClass]
+    public class ContainsSpecificSubstringTests
+    {
+        [TestMethod]
+        public void ContainsSpecificSubstring_basicstring_doContains()
+        {
+            Assert.IsTrue(ContainsSpecificSubstring_Exercice.ContainsSpecificSubstring("allez courage, tu t'en sors bien", "courage"));
+        }
+
+        [TestMethod]
+        public void ContainsSpecificSubstring_basicstring_doNotContains()
+        {
+            Assert.IsFalse(ContainsSpecificSubstring_Exercice.ContainsSpecificSubstring("allez courage, tu t'en sors bien", "soleil"));
+        }
+    }
+}
