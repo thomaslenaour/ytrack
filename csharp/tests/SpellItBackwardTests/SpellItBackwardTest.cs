01b737d982b1681e9ed38cdb98376c00e664dbdd
Gauthier <gauthier.michon@ynov.com>
2021-07-13 10:48:20 +0200

    SpellItBackwards

---
+++ csharp/tests/SpellItBackwardTests/SpellItBackwardTest.cs
@@ -0,0 +1,16 @@
+﻿using student.Quest01;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests
+{
+    [TestClass]
+    public class SpellItBackwardTests
+    {
+        [TestMethod]
+        public void SpellItBackward_HelloWorld_HelloWorldReversed()
+        {
+            //"helloworld" reçu, on retourne "dlrowolleh"
+            Assert.AreEqual("dlrowolleh", SpellItBackward_Exercice.SpellItBackward("helloworld"), "La méthode doit retourner \"dlrowolleh\" pour \"helloworld\" en entrée");
+        }
+    }
+}
