234e4e9f668bb495af168aa17e820673ad2306d4
Gauthier <gauthier.michon@ynov.com>
2021-07-13 11:47:57 +0200

    BeginsWithSpecificCharacter

---
+++ csharp/tests/BeginsWithSpecificCharacterTests/BeginsWithSpecificCharacterTest.cs
@@ -0,0 +1,21 @@
+﻿using CSharpDiscovery.Quest01;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests
+{
+    [TestClass]
+    public class BeginsWithSpecificCharacterTests
+    {
+        [TestMethod]
+        public void BeginsWithSpecificCharacter_basic_doStart()
+        {
+            Assert.IsTrue(BeginsWithSpecificCharacter_Exercice.BeginsWithSpecificCharacter("alexandre", 'a'));
+        }
+
+        [TestMethod]
+        public void BeginsWithSpecificCharacter_basic_doNotStart()
+        {
+            Assert.IsFalse(BeginsWithSpecificCharacter_Exercice.BeginsWithSpecificCharacter("alexandre", 'g'));
+        }
+    }
+}
