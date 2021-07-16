5676f876b55cf3a0b3d6a168055a29ebdfc13a9f
Gauthier <gauthier.michon@ynov.com>
2021-07-16 14:24:51 +0200

    FindLastIndex

---
+++ csharp/tests/FindLastIndexTests/FindLastIndexTest.cs
@@ -0,0 +1,36 @@
+﻿using CSharpDiscovery.Quest02;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests
+{
+    [TestClass]
+    public class FindLastIndexTests
+    {
+        [TestMethod]
+        public void FindLastIndex_OnlyOneOccurence_1()
+        {
+            var tab = new int[] { 1, 2, 3, 4, 5 };
+            Assert.AreEqual(1, FindLastIndex_Exercice.FindLastIndex(tab, 2));
+        }
+
+        [TestMethod]
+        public void FindLastIndex_TwoOccurences_4()
+        {
+            var tab = new int[] { 1, 2, 3, 4, 2 };
+            Assert.AreEqual(4, FindLastIndex_Exercice.FindLastIndex(tab, 2));
+        }
+
+        [TestMethod]
+        public void FindLastIndex_NoOccurence_Null()
+        {
+            var tab = new int[] { 1, 2, 3, 4, 5 };
+            Assert.AreEqual(null, FindLastIndex_Exercice.FindLastIndex(tab, 17));
+        }
+
+        [TestMethod]
+        public void FindLastIndex_NoTable_Null()
+        {
+            Assert.AreEqual(null, FindLastIndex_Exercice.FindLastIndex(null, 17));
+        }
+    }
+}
