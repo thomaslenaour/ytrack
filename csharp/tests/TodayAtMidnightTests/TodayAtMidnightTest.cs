16db164565e5528a6532ae5cfd319ffeacf3744c
Romain Ranaivoson <romain.ranaivoson@ynov.com>
2021-07-16 11:27:27 +0200

    added todayatidnigth test

---
+++ csharp/tests/TodayAtMidnightTests/TodayAtMidnightTest.cs
@@ -0,0 +1,17 @@
+﻿using CSharpDiscovery.Quest02;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace tests
+{
+    [TestClass]
+    public class TodayAtMidnightTests
+    {
+
+        [TestMethod]
+        public void TodayAtMidnight()
+        {
+            Assert.AreEqual(DateTime.Today, TodayAtMidnight_Exercice.TodayAtMidnight(), "Le retour attendu pour cette méthode est " + DateTime.Today.ToString());
+        }
+    }
+}
