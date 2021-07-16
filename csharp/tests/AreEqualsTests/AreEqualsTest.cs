1b2ec4b764ddef6a27231f6e485eeb6f335f91d4
Gauthier <gauthier.michon@ynov.com>
2021-07-16 14:33:01 +0200

    SortTable

---
+++ csharp/tests/AreEqualsTests/AreEqualsTest.cs
@@ -0,0 +1,38 @@
+﻿using CSharpDiscovery.Quest02;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tests
+{
+    [TestClass]
+    public class AreEqualsTests
+    {
+        [TestMethod]
+        public void AreEquals_SameTable_True()
+        {
+            var tab = new int[] { 1, 2, 3, 4, 5 };
+
+            Assert.IsTrue(AreEquals_Exercice.AreEquals(tab, tab));
+        }
+
+        [TestMethod]
+        public void AreEquals_DifferentTable_False()
+        {
+            var tab1 = new int[] { 1, 2, 3, 4, 5 };
+            var tab2 = new int[] { 1, 2, 3, 4, 6 };
+
+            Assert.IsFalse(AreEquals_Exercice.AreEquals(tab1, tab2));
+        }
+
+        [TestMethod]
+        public void AreEquals_DifferentSize_False()
+        {
+            var tab1 = new int[] { 1, 2, 3, 4, 5 };
+            var tab2 = new int[] { 1, 2, 3, 4 };
+
+            Assert.IsFalse(AreEquals_Exercice.AreEquals(tab1, tab2));
+        }
+    }
+}
---
+++ csharp/tests/SortTableTests/SortTableTest.cs
@@ -0,0 +1,26 @@
+﻿using CSharpDiscovery.Quest02;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace tests
+{
+    [TestClass]
+    public class SortTableTests
+    {
+        [TestMethod]
+        public void SortTable_UnorderedTable_TableSorted()
+        {
+            var tab = new int[] { 1, 5, 2, 4, 3 };
+            var sortedTab = new int[] { 1, 2, 3, 4, 5 };
+            Assert.IsTrue(sortedTab.SequenceEqual(SortTable_Exercice.SortTable(tab)));
+        }
+
+        [TestMethod]
+        public void SortTable_OrderedTable_TableSorted()
+        {
+            var sortedTab = new int[] { 1, 2, 3, 4, 6 };
+            Assert.IsTrue(sortedTab.SequenceEqual(SortTable_Exercice.SortTable(sortedTab)));
+        }
+    }
+}
