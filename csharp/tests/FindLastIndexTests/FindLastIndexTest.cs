ea7d8672d6e71d6d8d7fc087280543e87e4e0d31
Romain Ranaivoson <romain.ranaivoson@ynov.com>
2021-07-27 09:55:47 +0200

    fix bidon

--- csharp/tests/FindLastIndexTests/FindLastIndexTest.cs
+++ csharp/tests/FindLastIndexTests/FindLastIndexTest.cs
@@ -23,7 +23,7 @@ namespace tests
         [TestMethod]
         public void FindLastIndex_NoOccurence_Null()
         {
-            var tab = new int[] { 1, 2, 3, 4, 5 };
+            var tab = new int[] { 1, 2, 3, 4, 5, 6 };
             Assert.AreEqual(null, FindLastIndex_Exercice.FindLastIndex(tab, 17));
         }
 
