5b695cd86c281a59013afd0cf165c0778e2683cd
Gauthier <gauthier.michon@ynov.com>
2021-08-03 10:03:54 +0200

    arithmetic operator tester

--- php/tests/arithmetic-operators_test.php
+++ php/tests/arithmetic-operators_test.php
@@ -5,5 +5,5 @@ $tests = [];
 $tests[] = static fn ($eq): bool => $eq($firstSum, 135);
 $tests[] = static fn ($eq): bool => $eq($firstSub, 65);
 $tests[] = static fn ($eq): bool => $eq($firstMult, 3500);
-$tests[] = static fn ($eq): bool => $eq($firstDivision, 2.85714285714);
+$tests[] = static fn ($eq): bool => $eq($firstDivision, 2.8571428571429);
 $tests[] = static fn ($eq): bool => $eq($firstModulus, 30);
