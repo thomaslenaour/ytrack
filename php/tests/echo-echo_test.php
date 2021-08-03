db3e6d64f58b80612f0136ea030c38198a9865b4
Romain Ranaivoson <romain.ranaivoson@ynov.com>
2021-08-03 11:11:47 +0200

    tableau content

--- php/tests/echo-echo_test.php
+++ php/tests/echo-echo_test.php
@@ -2,11 +2,12 @@
 
 $tests = [];
 
-$fileContent = executeFile('echo-echo',false);
+$fileContent = executeFile('echo-echo');
 
 $tests[] = static fn ($eq): bool => count($fileContent) > 0;
 $tests[] = static fn ($eq, $exerciseName): bool => $eq(countLinesInFile($exerciseName), 3);
 
 $tests[] = static fn ($eq): bool => $eq($myVariable, 12);
+echo "$fileContent";
 $tests[] = static fn ($eq): bool => $eq($fileContent[0], 'This is my first echo !');
 $tests[] = static fn ($eq): bool => $eq($fileContent[1], 'My variable : 12');
