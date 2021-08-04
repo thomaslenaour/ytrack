0343c0aa6f815baa2ae0a455d96f756baba0e1c5
Gauthier <gauthier.michon@ynov.com>
2021-08-04 15:25:52 +0200

    correction test string-manipulator

--- php/tests/string-manipulator_test.php
+++ php/tests/string-manipulator_test.php
@@ -8,8 +8,8 @@ $tests[] = static fn ($eq): bool => $eq(capsMe("cappppps"), 'CAPPPPPS');
 $tests[] = static fn ($eq): bool => $eq(lowerMe("I'M CLINT"), "i'm clint");
 $tests[] = static fn ($eq): bool => $eq(lowerMe("I don't speak SPANish"), "i don't speak spanish");
 
-$tests[] = static fn ($eq): bool => $eq(upperCase("hello My Name is Quentin"), "Hello My Name Is Quentin");
-$tests[] = static fn ($eq): bool => $eq(upperCase("okkkkey let's go"), "Okkkkey Let's Go");
+$tests[] = static fn ($eq): bool => $eq(upperCaseFirst("hello My Name is Quentin"), "Hello My Name Is Quentin");
+$tests[] = static fn ($eq): bool => $eq(upperCaseFirst("okkkkey let's go"), "Okkkkey Let's Go");
 
 $tests[] = static fn ($eq): bool => $eq(lowerCaseFirst("Yeah Babe"), "yeah babe");
 $tests[] = static fn ($eq): bool => $eq(lowerCaseFirst("i'm So tied"), "i'm so tied");
