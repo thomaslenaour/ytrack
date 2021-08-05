38dd41dc140133f4e091eda95a74defddac62578
Gauthier <gauthier.michon@ynov.com>
2021-08-05 13:52:50 +0200

    recursive test

--- php/tests/recursive_test.php
+++ php/tests/recursive_test.php
@@ -1,5 +1,10 @@
 <?php
 
+function newFactorial(int $number): float
+{
+    return $number * newFactorial($number - 1);
+}
+
 $tests = [];
 
 $reflection = new ReflectionFunction('factorial');
@@ -9,11 +14,11 @@ $tests[] = static fn ($eq): bool => $eq($reflection->getNumberOfParameters(), 1)
 $tests[] = static fn ($eq): bool => $reflection->hasReturnType();
 $tests[] = static fn ($eq): bool => str_contains($content, 'factorial'); // Check recursively
 
-$tests[] = static fn ($eq): bool => $eq(factorial(1), 1.0);
-$tests[] = static fn ($eq): bool => $eq(factorial(2), 2.0);
-$tests[] = static fn ($eq): bool => $eq(factorial(3), 6.0);
-$tests[] = static fn ($eq): bool => $eq(factorial(4), 24.0);
-$tests[] = static fn ($eq): bool => $eq(factorial(5), 120.0);
-$tests[] = static fn ($eq): bool => $eq(factorial(6), 720.0);
-$tests[] = static fn ($eq): bool => $eq(factorial(45), 1.1962222086548E+56);
+$tests[] = static fn ($eq): bool => $eq(factorial(1), 1);
+$tests[] = static fn ($eq): bool => $eq(factorial(2), 2);
+$tests[] = static fn ($eq): bool => $eq(factorial(3), 6);
+$tests[] = static fn ($eq): bool => $eq(factorial(4), 24);
+$tests[] = static fn ($eq): bool => $eq(factorial(5), 120);
+$tests[] = static fn ($eq): bool => $eq(factorial(6), 720);
+$tests[] = static fn ($eq): bool => $eq(factorial(45), newFactorial(45));
 $tests[] = static fn ($eq): bool => $eq(factorial(1000), INF);
