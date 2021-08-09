be573caf738fb8e0923b3398b979527cbd3d51a0
Romain Ranaivoson <romain.ranaivoson@ynov.com>
2021-08-09 09:55:11 +0200

    FINISHED CrateCar, Accelerate and Brake et CreateT

--- csharp/tests/CreateTruckTests/CreateTruckTest.cs
+++ csharp/tests/CreateTruckTests/CreateTruckTest.cs
@@ -11,7 +11,7 @@ namespace tests
     public class CreateTruckTests
     {
 
-        public static object instance;
+        public static List<object> Instances = new List<object>();
 
         [TestMethod]
         public void CheckInherance()
@@ -41,7 +41,7 @@ namespace tests
             try
             {
                 ConstructorInfo ctor = typeof(Truck).GetConstructor(Type.EmptyTypes);
-                instance = ctor.Invoke(new object[] { });
+                Instances.Add(ctor.Invoke(new object[] { }));
             }
             catch (System.Exception)
             {
@@ -49,20 +49,20 @@ namespace tests
                 throw;
             }
 
-            Assert.AreEqual(0, TestUtils.GetValueFromInstance(instance, "Tonnage"), "Values are not affected correctly !");
-            Assert.AreEqual("Unknown", TestUtils.GetValueFromInstance(instance, "Brand"), "Values are not affected correctly !");
-            Assert.AreEqual("Unknown", TestUtils.GetValueFromInstance(instance, "Color"), "Values are not affected correctly !");
-            Assert.AreEqual(0, TestUtils.GetValueFromInstance(instance, "CurrentSpeed"), "Values are not affected correctly !");
+            Assert.AreEqual(0, TestUtils.GetValueFromInstance(Instances[0], "Tonnage"), "Values are not affected correctly !");
+            Assert.AreEqual("Unknown", TestUtils.GetValueFromInstance(Instances[0], "Brand"), "Values are not affected correctly !");
+            Assert.AreEqual("Unknown", TestUtils.GetValueFromInstance(Instances[0], "Color"), "Values are not affected correctly !");
+            Assert.AreEqual(0, TestUtils.GetValueFromInstance(Instances[0], "CurrentSpeed"), "Values are not affected correctly !");
         }
 
         [TestMethod]
         public void CheckToString1()
         {
-            Assert.AreEqual("Unknown Unknown 0T Truck", TestUtils.CallMethod(instance, "ToString"));
+            Assert.AreEqual("Unknown Unknown 0T Truck", TestUtils.CallMethod(Instances[0], "ToString"));
         }
 
         [TestMethod]
-        public void Check4ParamsConstructor1()
+        public void Check4ParamsConstructor()
         {
 
             try
@@ -70,7 +70,8 @@ namespace tests
                 ConstructorInfo ctor = typeof(Truck).GetConstructor(new[] {
                     typeof(Int32), typeof(String), typeof(String), typeof(Int32)
             });
-                instance = ctor.Invoke(new object[] { 2, "MAC", "White", 80 });
+                Instances.Add(ctor.Invoke(new object[] { 2, "MAC", "White", 80 }));
+                Instances.Add(ctor.Invoke(new object[] { 3, "Renault", "Black", 20 }));
             }
             catch (System.Exception)
             {
@@ -78,43 +79,57 @@ namespace tests
                 throw;
             }
 
-            Assert.AreEqual(2, TestUtils.GetValueFromInstance(instance, "Tonnage"), "Values are not affected correctly !");
-            Assert.AreEqual("MAC", TestUtils.GetValueFromInstance(instance, "Brand"), "Values are not affected correctly !");
-            Assert.AreEqual("White", TestUtils.GetValueFromInstance(instance, "Color"), "Values are not affected correctly !");
-            Assert.AreEqual(80, TestUtils.GetValueFromInstance(instance, "CurrentSpeed"), "Values are not affected correctly !");
+            // MAC White 80
+            Assert.AreEqual(2, TestUtils.GetValueFromInstance(Instances[1], "Tonnage"), "Values are not affected correctly !");
+            Assert.AreEqual("MAC", TestUtils.GetValueFromInstance(Instances[1], "Brand"), "Values are not affected correctly !");
+            Assert.AreEqual("White", TestUtils.GetValueFromInstance(Instances[1], "Color"), "Values are not affected correctly !");
+            Assert.AreEqual(80, TestUtils.GetValueFromInstance(Instances[1], "CurrentSpeed"), "Values are not affected correctly !");
+
+            // Renault Black 20
+            Assert.AreEqual(3, TestUtils.GetValueFromInstance(Instances[2], "Tonnage"), "Values are not affected correctly !");
+            Assert.AreEqual("Renault", TestUtils.GetValueFromInstance(Instances[2], "Brand"), "Values are not affected correctly !");
+            Assert.AreEqual("Black", TestUtils.GetValueFromInstance(Instances[2], "Color"), "Values are not affected correctly !");
+            Assert.AreEqual(20, TestUtils.GetValueFromInstance(Instances[2], "CurrentSpeed"), "Values are not affected correctly !");
         }
 
         [TestMethod]
         public void CheckToString2()
         {
-            Assert.AreEqual("White MAC 2T Truck", TestUtils.CallMethod(instance, "ToString"));
+            Assert.AreEqual("White MAC 2T Truck", TestUtils.CallMethod(Instances[1], "ToString"));
+            Assert.AreEqual("Black Renault 3T Truck", TestUtils.CallMethod(Instances[2], "ToString"));
+        }
+
+        [TestMethod]
+        public void CheckAccelerate()
+        {
+            Assert.IsTrue(TestUtils.HasMethod<Truck>("Accelerate", new Type[] { typeof(int) }), "Method Accelerate not found");
         }
         [TestMethod]
-        public void Check4ParamsConstructor2()
+        public void CheckBrake()
         {
-
-            try
-            {
-                ConstructorInfo ctor = typeof(Truck).GetConstructor(new[] {
-                    typeof(Int32), typeof(String), typeof(String), typeof(Int32)
-            });
-                instance = ctor.Invoke(new object[] { 3, "Renault", "Black", 20 });
-            }
-            catch (System.Exception)
-            {
-                Assert.Fail("Cannot call the constructor with 4 parameters");
-                throw;
-            }
-
-            Assert.AreEqual(3, TestUtils.GetValueFromInstance(instance, "Tonnage"), "Values are not affected correctly !");
-            Assert.AreEqual("Renault", TestUtils.GetValueFromInstance(instance, "Brand"), "Values are not affected correctly !");
-            Assert.AreEqual("Black", TestUtils.GetValueFromInstance(instance, "Color"), "Values are not affected correctly !");
-            Assert.AreEqual(20, TestUtils.GetValueFromInstance(instance, "CurrentSpeed"), "Values are not affected correctly !");
+            Assert.IsTrue(TestUtils.HasMethod<Truck>("Brake", new Type[] { typeof(int) }), "Method Brake not found");
         }
 
-        public void CheckToString3()
+        [TestMethod]
+        public void Chech30KMh()
         {
-            Assert.AreEqual("Black Renault 3T Truck", TestUtils.CallMethod(instance, "ToString"));
+
+            TestUtils.CallMethod(Instances[0], "Accelerate", new object[] { 30 });
+            Assert.AreEqual(30, TestUtils.GetValueFromInstance(Instances[0], "CurrentSpeed"), "Value is not equal to 30 Km/h !");
+        }
+
+        [TestMethod]
+        public void Check100KMh()
+        {
+            TestUtils.CallMethod(Instances[1], "Accelerate", new object[] { 40 });
+            Assert.AreEqual(100, TestUtils.GetValueFromInstance(Instances[1], "CurrentSpeed"), "Value is not equal to 100 Km/h or you are going too fast !");
+        }
+
+        [TestMethod]
+        public void CheckNega()
+        {
+            TestUtils.CallMethod(Instances[2], "Brake", new object[] { 30 });
+            Assert.AreEqual(0, TestUtils.GetValueFromInstance(Instances[2], "CurrentSpeed"), "Value is not equal to 0 Km/h or you are going backwards !");
         }
     }
 }
