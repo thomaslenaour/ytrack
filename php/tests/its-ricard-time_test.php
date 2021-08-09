abd5480dbb695702759ae146f6ff02109310c907
Romain Ranaivoson <romain.ranaivoson@ynov.com>
2021-08-09 11:49:29 +0200

    fixed its ricard time ...

--- php/tests/its-ricard-time_test.php
+++ php/tests/its-ricard-time_test.php
@@ -15,4 +15,4 @@ $result = executeFile('its-ricard-time');
 $tests[] = static fn ($eq): bool =>
 $result[0] === "Come on ricard number 1" && $result[1] === "Come on ricard number 2" && $result[2] === "Come on ricard number 3" &&
     $result[3] === "I'll have to stop soon!" && $result[4] === "Come on ricard number 4" && $result[5] === "Come on ricard number 5" &&
-    $result[6] === "I am no longer very fresh ..." && $result[7] === "Come on ricard number 6" && $result[8] === "Come on ricard number 7";
+    $result[6] === "I am no longer very fresh ..." || $result[6] === "I am no longer very fresh …" && $result[7] === "Come on ricard number 6" && $result[8] === "Come on ricard number 7";
