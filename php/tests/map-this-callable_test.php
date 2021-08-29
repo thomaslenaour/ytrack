bc1e2b3e575fcb815221e994ef37e8ba5569e30c
Romain Ranaivoson <romain.ranaivoson@ynov.com>
2021-08-29 15:56:41 +0200

    Fix map this callable test

--- php/tests/map-this-callable_test.php
+++ php/tests/map-this-callable_test.php
@@ -5,7 +5,7 @@ $tests = [];
 $reflection = new ReflectionFunction('myArrayMap');
 $functionContent = getFunctionContent($reflection);
 
-$tests[] = static fn ($eq): bool => $eq($reflection->getNumberOfParameters(), 3);
+$tests[] = static fn ($eq): bool => $eq($reflection->getNumberOfParameters(), 2);
 $tests[] = static fn ($eq): bool => $eq($reflection->getNumberOfRequiredParameters(), 2);
 $tests[] = static fn ($eq): bool => $eq(str_contains($functionContent, "array_map"), false);
 $tests[] = static fn ($eq): bool => $eq(str_contains($functionContent, "array_map_recursive"), false);
@@ -20,17 +20,11 @@ $tests[] = static fn ($eq): bool => $eq(
     [2, 4, 6, 8, 10]
 );
 
+$tests[] = static fn ($eq): bool => $eq(myArrayMap(null, []), []);
+
 $tests[] = static fn ($eq): bool => $eq(myArrayMap(null, [1, 3, 7]), [1, 3, 7]);
-$tests[] = static fn ($eq): bool => $eq(
-    myArrayMap(null, [1, 2, 3], ['one', 'two', 'three'], ['uno', 'dos', 'tres']),
-    [
-        [1, 'one', 'uno'],
-        [2, 'two', 'dos'],
-        [3, 'three', 'tres']
-    ]
-);
 
 $tests[] = static fn ($eq): bool => $eq(
-    myArrayMap(static fn ($n) => $n['value'], ['value1' => 1, 'value2' => 2, 'value3' => 3]),
-    [1, 2 ,3]
+    myArrayMap(static fn ($n) => $n['value'], [['value' => 1], ['value' => 2], ['value' => 3]]),
+    [1, 2, 3]
 );
